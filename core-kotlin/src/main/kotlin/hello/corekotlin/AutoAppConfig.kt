e3c878e25daf19d0cf46972f53c03065c4d94f9d
devjunku <junku8782@gmail.com>
2023-08-05 13:48:01 +0900

    java-to-kotlin Refactoring Bean Test AutoConfig

--- core-kotlin/src/main/kotlin/hello/corekotlin/AutoAppConfig.kt
+++ core-kotlin/src/main/kotlin/hello/corekotlin/AutoAppConfig.kt
@@ -1,4 +1,10 @@
 package hello.corekotlin
 
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan
 class AutoAppConfig {
+
 }
--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,4 +1,11 @@
 package hello.core;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
 public class AutoAppConfig {
+
+
+
 }
