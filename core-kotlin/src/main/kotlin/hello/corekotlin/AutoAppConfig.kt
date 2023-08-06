df90099bdf1fa6144c0fd21f212662628a9694ca
devjunku <junku8782@gmail.com>
2023-08-06 22:08:52 +0900

    java-to-kotlin Refactoring & Coroutines

--- core-kotlin/src/main/kotlin/hello/corekotlin/AutoAppConfig.kt
+++ core-kotlin/src/main/kotlin/hello/corekotlin/AutoAppConfig.kt
@@ -1,10 +1,24 @@
 package hello.corekotlin
 
+
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.FilterType
+
+/**
+ * kotlin의 경우 @ComponentScan의 정보를 arrayOf()에 담아서 보내준다.
+ */
+
 
 @Configuration
-@ComponentScan
+@ComponentScan(
+    excludeFilters = arrayOf(
+        ComponentScan.Filter(type = FilterType.ANNOTATION),
+        ComponentScan.Filter(Configuration::class)
+    )
+)
 class AutoAppConfig {
+
+    
 
 }
--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,9 +1,13 @@
 package hello.core;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // scan할 때 빼는 거 지정
+)
 public class AutoAppConfig {
 
 
